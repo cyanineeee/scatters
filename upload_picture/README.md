89164f9465d4fdaadd31159ffb28b850ac6ad95f
cyanineeee <sapphire_ruby@163.com>
2024-10-30 13:21:08 +0800

    setup nextjs branch

--- upload_picture/README.md
+++
@@ -1,1 +0,0 @@
-### please change the MEDIA_ROOT before start, which are located in mysite/settings.py. Otherwise it will be some errors.
--- upload_picture/display/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UploadImages(models.Model):
-    imagename = models.CharField(max_length=255)
-    images = models.ImageField()
-    def get_img_name(self):
-        return self.imagename
