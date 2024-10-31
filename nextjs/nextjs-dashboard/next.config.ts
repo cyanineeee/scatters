a1cb06c364ce7ec086d2b8bf9a24c37466020bf6
cyanineeee <sapphire_ruby@163.com>
2024-10-31 14:14:27 +0800

    chapter-11 end

--- nextjs/nextjs-dashboard/next.config.ts
+++ nextjs/nextjs-dashboard/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // because the nextjs version is not suitable, which requires nextjs c 
+  // experimental : {
+  //   ppr:'incremental',
+  // }
 };
 
 export default nextConfig;
