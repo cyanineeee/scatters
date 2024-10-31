f91514d3238bcc2dea637bb6d7ba5206942d5a49
cyanineeee <sapphire_ruby@163.com>
2024-10-31 13:11:35 +0800

    chapter-11 end

--- nextjs/nextjs-dashboard/app/ui/search.tsx
+++ nextjs/nextjs-dashboard/app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams,usePathname,useRouter } from 'next/navigation';
+import { useDebouncedCallback  } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
@@ -9,15 +10,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter()
 
   
-  function handleSearch(term:string) {
+  const handleSearch = useDebouncedCallback((term:string) => {
     //URLSearchParams is a Web API 
     const params = new URLSearchParams(searchParams)
+    params.set('page','1');
     if(term){
       params.set('query',term);
     }else{
       params.delete('query')
     }
-  }
+    replace(`${pathname}?${params.toString()}`)
+  },300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -30,6 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
