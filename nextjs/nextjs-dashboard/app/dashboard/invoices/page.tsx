e366d286fdba246757bc8ef8f2666253d6b77b93
cyanineeee <sapphire_ruby@163.com>
2024-10-31 13:41:05 +0800

    Revert "Merge branch 'next'"
    This reverts commit 3571a68bda36251a4aaadf5cbf015f711e9a9ffa, reversing
    changes made to e38e3ce7f8c6e33561600e735f31dff6fc943eb3.

--- nextjs/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,20 +5,8 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
  
-export default async function Page(props : {
-  searchParams?:Promise<{
-    query?:string;
-    page?:string
-  }>;
-}) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-
-  const totalPages = await fetchInvoicesPages(query);
-  
+export default async function Page() {
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -28,11 +16,11 @@ export default async function Page(props : {
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
-      </Suspense>
+      </Suspense> */}
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
+        {/* <Pagination totalPages={totalPages} /> */}
       </div>
     </div>
   );
--- nextjs/nextjs-dashboard/app/ui/search.tsx
+++ nextjs/nextjs-dashboard/app/ui/search.tsx
@@ -2,7 +2,6 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams,usePathname,useRouter } from 'next/navigation';
-import { useDebouncedCallback  } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
@@ -10,17 +9,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter()
 
   
-  const handleSearch = useDebouncedCallback((term:string) => {
+  function handleSearch(term:string) {
     //URLSearchParams is a Web API 
     const params = new URLSearchParams(searchParams)
-    params.set('page','1');
     if(term){
       params.set('query',term);
     }else{
       params.delete('query')
     }
-    replace(`${pathname}?${params.toString()}`)
-  },300)
+  }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -33,7 +30,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
