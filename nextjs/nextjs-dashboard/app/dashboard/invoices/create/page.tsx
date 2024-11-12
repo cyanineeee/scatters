c492d6ac0dbb78978b0f7270e7fafe55f30a911c
cyanineeee <sapphire_ruby@163.com>
2024-11-12 14:08:06 +0800

    resolve build bug

--- nextjs/nextjs-dashboard/app/dashboard/invoices/create/page.tsx
+++ nextjs/nextjs-dashboard/app/dashboard/invoices/create/page.tsx
@@ -4,7 +4,6 @@ import { fetchCustomers } from '@/app/lib/data';
 
 export default async function Page(){
     const customers = await fetchCustomers();
-
     return (
         <main>
             {/* 面包屑导航栏 */}
